d50908e7d494d6e62637053f95d39a1328392a7c
Yash-Gajewar <yashgajewar06@gmail.com>
2024-11-19 13:10:01 +0530

    version 1

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+document.getElementById('registerBtn').addEventListener('click', function () {
+    window.location.href = 'register.html';
+});
+
+document.getElementById('loginBtn').addEventListener('click', function () {
+    window.location.href = 'login.html'; 
+});
